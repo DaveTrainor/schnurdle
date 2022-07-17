e726e1f60c451438e8b1adfea5c4a7f182f6a9c8
DaveTrainor <davetrainor79@gmail.com>
2022-07-18 00:10:18 +0100

    add collect first_line, not working though

--- wordle_first_guess_finder.rb
+++ wordle_first_guess_finder.rb
@@ -23,9 +23,11 @@ post '/game_reader' do
     
     # puts params[:game1].dump
     game_string_dump = params[:game1].dump
-    puts game_string_dump
-    game_array = game_string_dump.match /(Wordle \d{1,3})/
-    puts game_array
-    erb:game_reader, :locals => {:game_ID=>game_array[1]}
+    puts "game_string_dump: #{game_string_dump}"
+    game_array = game_string_dump.match /(Wordle \d{1,3}).+(\\u\S+).+/
+    game_array_2 = game_string_dump.match /(\\u\S+)/
+    puts "game_array: #{game_array}"
+    puts "game_array[1]: #{game_array[1]}"
+    erb:game_reader, :locals => {:game_ID=>game_array[1], :first_line=>game_array[2]}
 
 end
