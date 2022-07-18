26156847af09a51e7e4f00d9562b3268028c8eb1
DaveTrainor <davetrainor79@gmail.com>
2022-07-18 23:40:37 +0100

    regex capture

--- wordle_first_guess_finder.rb
+++ wordle_first_guess_finder.rb
@@ -24,10 +24,13 @@ post '/game_reader' do
     # puts params[:game1].dump
     game_string_dump = params[:game1].dump
     puts "game_string_dump: #{game_string_dump}"
-    game_array = game_string_dump.match /(Wordle \d{1,3}).+(\\u\S+).+/
-    game_array_2 = game_string_dump.match /(\\u\S+)/
-    puts "game_array: #{game_array}"
-    puts "game_array[1]: #{game_array[1]}"
+    game_array = game_string_dump.match /(Wordle \d{1,3})\s+\S{2,4}\s+(\S+).+/
+    first_line_array = game_array[2].split ('\\')
+    puts first_line_array
+    translation = {"u2B1C": "grey", 'u{1F7E8}': 'yellow', 'u{1F7E9}': 'green'}
+    first_line_english = []
+    first_line_english = first_line_array.map {|square| translation[square]}
+    puts "first line english: #{first_line_english}"
     erb:game_reader, :locals => {:game_ID=>game_array[1], :first_line=>game_array[2]}
 
 end
