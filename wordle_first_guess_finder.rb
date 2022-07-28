0ce7075ff28d8470bc3a768d6d2a92a5e77318f5
dave-t-MT <david.trainor@madetech.com>
2022-07-28 17:18:41 +0100

    returns possible words to terminal

--- wordle_first_guess_finder.rb
+++ wordle_first_guess_finder.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative 'solutionlist'
+require_relative 'validwords'
 
 
 get '/' do
@@ -30,7 +31,7 @@ def first_line_english(game_array)
     first_line_colours = first_line_array[1..5].map {|square| translation[square.to_sym]}
 end
 
-def list_possible_guesses(solution, first_line_english)
+def list_possible_characters(solution, first_line_english)
     possible_chars = [('a'..'z').to_a]*5
     sol_arr = solution.scan /\w/
     first_line_english.each_with_index {|square, i| 
@@ -38,18 +39,31 @@ def list_possible_guesses(solution, first_line_english)
             possible_chars[i] = solution[i]
         elsif square =="yellow"
             possible_chars[i] = sol_arr
+            possible_chars[i].reject! {|char| char == sol_arr[i]}
         else 
             possible_chars[i].reject! {|char| sol_arr.index(char)}  
         
         end
         }
-        possible_chars
-        print possible_chars
-
-
-
-
+    possible_chars.each {|char| print "#{char} \n"}
+    return possible_chars
+    
+        
 end
+
+def find_matching_words(possible_chars)
+    word_list_array = Valid_words.word_list.split(" ")
+    
+    possible_words = word_list_array.select {|word| 
+        possible_chars[0].include?(word[0]) &&
+        possible_chars[1].include?(word[1]) &&
+        possible_chars[2].include?(word[2]) &&
+        possible_chars[3].include?(word[3]) &&
+        possible_chars[4].include?(word[4])}
+         # == /^[possible_chars[0]][possible_chars[1]][possible_chars[2]][possible_chars[3]][possible_chars[4]]$/}
+    print(possible_words)
+end
+
 
 
 
@@ -57,12 +71,14 @@ post '/game_reader' do
     
     game_string_dump = params[:game1].dump
     game_array = game_array(game_string_dump)
-    list_possible_guesses(get_solution(game_array[1]), first_line_english(game_array))
-      
+    poss_chars = list_possible_characters(get_solution(game_array[1]), first_line_english(game_array))
+    find_matching_words(poss_chars) 
+
     erb:game_reader, :locals => {
         :game_ID=>game_number(game_array), 
         :first_line=>first_line_english(game_array).to_s, 
-        :solution=>get_solution(game_array[1])
+        :solution=>get_solution(game_array[1]),
+        :poss_chars=>poss_chars
         
     }
 
